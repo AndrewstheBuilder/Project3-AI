869faec7cc3bd784e7b6b1d0852ba4b945773c46
Jonathan Shih <jonnyshih@gmail.com>
2022-03-23 09:28:43 -0400

    Added a few carriage returns to get rid of squiggly lines Mainly to start branch

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
 
+
 def selectFile():
     '''
     '''
     file = filedialog.askopenfilename(initialdir = "/", title=("Select a file"), filetypes=(("Text file", "*.txt*"),("Any File", "*.*")))
+
 
 def createGui():
     '''
@@ -16,11 +18,12 @@ def createGui():
     frm = ttk.Frame(gui, padding=10)
     frm.grid()
     ttk.Label(frm, text="Enter a file for Attributes:").grid(column=0, row=0)
-    #global attrLabel
+    # global attrLabel
     attrLabel = ttk.Label(frm,text="")
     attrLabel.grid(column=1, row=0)
     ttk.Button(frm, text="Browse", command=selectFile).grid(column=1, row=0)
     gui.mainloop()
+
 
 def main():
     createGui()
